6f3959856162704bde579c9b45dd45f5904ab6fc
B3ns44d <abdsamad23bnsaad@gmail.com>
2021-02-02 23:55:34 +0100

    setup firebase 🛢️

---
+++ src/contexts/firebase.jsx
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AuthDomain,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_ID,
+};
